924a9714710b7cc8414b0a089f32f40c0f6a3751
SpirinOleg <Spirin_O_E@mail.ru>
2021-05-19 15:20:58 +0300

    Push уведомление

--- app/src/main/java/com/example/moneybox/main/MainActivity.kt
+++ app/src/main/java/com/example/moneybox/main/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.moneybox.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +20,8 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.moneybox.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -26,12 +36,33 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        createNotificationChannel()
+
+
+
         setupActionBar(navController, appBarConfiguration)
     }
 
     private fun setupActionBar(navController: NavController,
                                appBarConfig : AppBarConfiguration) {
         setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -47,7 +78,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_refresh -> {
+                Toast.makeText(this, "Поступила сумма 1000 ₽", Toast.LENGTH_SHORT).show()
+                balance.text = "37549 ₽"
+
+                var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_add_alert_24)
+                    .setContentTitle("Инвестиционная копилка")
+                    .setContentText("На ваш счет поступила сумма в размере 1000 рублей")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                with(NotificationManagerCompat.from(this)) {
+                    // notificationId is a unique int for each notification that you must define
+                    notify(NOTIFICATION_ID, builder.build())
+                }
+            }
+        }
         return item.onNavDestinationSelected(navController)
                 || super.onOptionsItemSelected(item)
         }
+
+    companion object {
+        const val NOTIFICATION_ID = 101
+        const val CHANNEL_ID = "channelID"
+    }
 }
--- app/src/main/java/com/example/moneybox/main/MainFragment.kt
+++ app/src/main/java/com/example/moneybox/main/MainFragment.kt
@@ -8,10 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.moneybox.R
 import com.example.moneybox.feature.charts.PieData
-import com.example.moneybox.model.Briefcase
 import com.example.moneybox.model.Council
 import kotlinx.android.synthetic.main.item_briefcase_content.*
 import kotlinx.android.synthetic.main.item_briefcase_content.any_chart_vie
