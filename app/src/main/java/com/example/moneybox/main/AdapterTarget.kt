a016965e0e3f5376a44eed4cd4874565bb1c9cff
SpirinOleg <Spirin_O_E@mail.ru>
2021-05-20 15:00:31 +0300

    Добавлен переход по кнопке

--- app/src/main/java/com/example/moneybox/main/AdapterTarget.kt
+++ app/src/main/java/com/example/moneybox/main/AdapterTarget.kt
@@ -3,11 +3,13 @@ package com.example.moneybox.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.moneybox.R
+import kotlinx.android.synthetic.main.item_day_include.view.*
 
 class AdapterTarget: RecyclerView.Adapter<AdapterTarget.ViewHolder>()  {
 
@@ -38,9 +40,7 @@ class AdapterTarget: RecyclerView.Adapter<AdapterTarget.ViewHolder>()  {
         holder.nameTarget.text = targetList[position]
         holder.dateList.text = dateList[position]
         holder.percentTarget.progress = percentTarget[position]
-        holder.itemView.setOnClickListener{
-            it.findNavController().navigate(R.id.action_main_dest_to_placeholder_target)
-        }
+        holder.bind()
 
     }
 
@@ -53,6 +53,12 @@ class AdapterTarget: RecyclerView.Adapter<AdapterTarget.ViewHolder>()  {
         var nameTarget: TextView = itemView.findViewById(R.id.name_target)
         var dateList: TextView = itemView.findViewById(R.id.date_target)
         var percentTarget: ProgressBar = itemView.findViewById(R.id.progressBar)
+
+        fun bind(){
+            itemView.btn_target.setOnClickListener {
+                it.findNavController().navigate(R.id.action_main_dest_to_placeholder_target)
+            }
+        }
 
     }
 }
